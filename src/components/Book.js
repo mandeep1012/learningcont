ddfbdf0caed50d9be60a76515f53cd0e20341069
mandeep1012 <30328595+mandeep1012@users.noreply.github.com>
2019-08-13 16:28:32 +0530

    'learning2'

---
+++ src/components/Book.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Book = () => <h3>Hello From Book </h3>;
+export default Book;
---
+++ src/components/Booklist.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Book from './Book';
+
+ const Booklist = () => (
+  <div>
+    <h2>Hellow</h2>
+    <Book />
+  </div>
+);
+
+export default Booklist;
